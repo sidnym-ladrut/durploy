c41ecd051f8e8ed84f68e5d36c286d32f6aa3649
Sidnym Ladrut <sidnym@ladrut.xyz>
2023-08-19 03:11:39 +0000

    durploy-ship: add very basic support for init scripts

---
+++ examples/squad.sh
@@ -0,0 +1,24 @@
+_durploy__desk_init squad "$durploy_ship_pier"
+
+_durploy__desk_cgit squad "$durploy_ship_pier" \
+	git@github.com:urbit/docs-examples.git "" \
+	"groups-app/full-desk/" "./"
+
+_durploy__desk_cgit squad "$durploy_ship_pier" \
+	git@github.com:urbit/urbit.git "" \
+	"pkg/arvo/mar/{bill*,hoon*,json*,kelvin*,mime*,noun*,ship*,txt*}" "mar/"
+_durploy__desk_cgit squad "$durploy_ship_pier" \
+	git@github.com:urbit/urbit.git "" \
+	"pkg/arvo/lib/{agentio*,dbug*,default-agent*,skeleton*}" "lib/"
+
+_durploy__desk_cgit squad "$durploy_ship_pier" \
+	git@github.com:tloncorp/landscape.git "" \
+	"desk/mar/docket*" "mar/"
+_durploy__desk_cgit squad "$durploy_ship_pier" \
+	git@github.com:tloncorp/landscape.git "" \
+	"desk/lib/docket*" "lib/"
+_durploy__desk_cgit squad "$durploy_ship_pier" \
+	git@github.com:tloncorp/landscape.git "" \
+	"desk/sur/docket*" "sur/"
+
+_durploy__desk_wrap squad "$durploy_ship_pier"
