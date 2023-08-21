35deaf75b3890f48220a3f2094b0e841987c6a22
Sidnym Ladrut <sidnym@ladrut.xyz>
2023-08-21 19:24:30 +0000

    durploy: big renaming/refactoring in preparation for better init script sandboxing

--- examples/squad.sh
+++ examples/squad.sh
@@ -1,16 +1,16 @@
-_durploy__desk_init squad "$durploy_ship_pier"
+_durploy__p__desk_init "$durploy_ship_pier" squad
 
-_durploy__desk_cgit squad "$durploy_ship_pier" \
+_durploy__p__desk_cpgit "$durploy_ship_pier" squad \
 	git@github.com:urbit/docs-examples.git "" \
 	"groups-app/full-desk/ ./"
-_durploy__desk_cgit squad "$durploy_ship_pier" \
+_durploy__p__desk_cpgit "$durploy_ship_pier" squad \
 	git@github.com:urbit/urbit.git "" \
 	"pkg/arvo/mar/{bill*,hoon*,json*,kelvin*,mime*,noun*,ship*,txt*} mar/" \
 	"pkg/arvo/lib/{agentio*,dbug*,default-agent*,skeleton*} lib/"
-_durploy__desk_cgit squad "$durploy_ship_pier" \
+_durploy__p__desk_cpgit "$durploy_ship_pier" squad \
 	git@github.com:tloncorp/landscape.git "" \
 	"desk/mar/docket* mar/" \
 	"desk/lib/docket* lib/" \
 	"desk/sur/docket* sur/"
 
-_durploy__desk_wrap squad "$durploy_ship_pier"
+_durploy__p__desk_inst "$durploy_ship_pier" squad
