976f52b21ce38f80e137c9c75d6883e3e88d33da
Sidnym Ladrut <sidnym@ladrut.xyz>
2023-08-21 20:56:29 +0000

    durploy-ship: add command aliasing to simplify syntax of init scripts

--- examples/squad.sh
+++ examples/squad.sh
@@ -1,16 +1,10 @@
-_durploy__p__desk_init "$durploy_ship_pier" squad
-
-_durploy__p__desk_cpgit "$durploy_ship_pier" squad \
-	git@github.com:urbit/docs-examples.git "" \
-	"groups-app/full-desk/ ./"
-_durploy__p__desk_cpgit "$durploy_ship_pier" squad \
-	git@github.com:urbit/urbit.git "" \
+desk_init squad
+desk_cpgit squad git@github.com:urbit/docs-examples.git "" "groups-app/full-desk/ ./"
+desk_cpgit squad git@github.com:urbit/urbit.git "" \
 	"pkg/arvo/mar/{bill*,hoon*,json*,kelvin*,mime*,noun*,ship*,txt*} mar/" \
 	"pkg/arvo/lib/{agentio*,dbug*,default-agent*,skeleton*} lib/"
-_durploy__p__desk_cpgit "$durploy_ship_pier" squad \
-	git@github.com:tloncorp/landscape.git "" \
+desk_cpgit squad git@github.com:tloncorp/landscape.git "" \
 	"desk/mar/docket* mar/" \
 	"desk/lib/docket* lib/" \
 	"desk/sur/docket* sur/"
-
-_durploy__p__desk_inst "$durploy_ship_pier" squad
+desk_inst squad
